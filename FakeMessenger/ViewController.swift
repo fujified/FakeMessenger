2456923012167014ead5769a6f995fd0ed9f89c6
fujified <uktras@gmail.com>
2021-01-24 20:33:59 +0300

    Initial Commit

---
+++ FakeMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeMessenger
+//
+//  Created by mr spock on 1/24/21.
+//  Copyright © 2021 mr spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
